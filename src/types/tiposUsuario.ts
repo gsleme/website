7708551b261872a8220d2ce94a0d4f877982497e
FelipeFerrete <felipeferretelemes@gmail.com>
2025-11-23 15:52:11 -0300

    :recycle: refactor: Tipagem + AuthContex | Usuarios

--- src/types/tiposUsuario.ts
+++ src/types/tiposUsuario.ts
@@ -1,15 +1,18 @@
 export type tipoUsuarioForm = {
-    nome:string;
-    username:string;
-    email:string;
-    senha:string;
-    confirmarSenha:string;
+    nome: string;
+    username: string;
+    email: string;
+    senha: string;
+    confirmarSenha: string;
+    area: string;
+    acessibilidade: string;
 }
 
 export type tipoTokenInfo = {
-    id:string;
-    nome:string;
-    username:string;
-    email:string;
+    id: string;
+    area: string;
+    acessibilidade: string;
+    modulosConcluidos: number;
+    tempoPlataformaDias: number;
     exp: number;
 }
