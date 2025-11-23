b9412c2f33a944f7970cf5d7dbb9d38dce61ff92
Gustavo-Bosak <gustavobosaksantos@gmail.com>
2025-11-23 04:00:53 -0300

    :sparkles: feat: adiciona página de módulo
    Ajustes gerais em trilhas, ajustes de classes utilitárias de estilo

--- src/routes/Modulo/index.tsx
+++ src/routes/Modulo/index.tsx
@@ -1,7 +1,62 @@
-function Modulo() {
+import { Link, useParams } from 'react-router-dom'
+
+import image from '../../assets/images/image.png'
+
+import { FaAward } from 'react-icons/fa'
+
+function Modulo () {
+  const { modulo } = useParams()
+
   return (
     <main>
-        
+      <section className='flex justify-center w-full py-8 relative'>
+        <div className='h-[270%] -translate-y-1/2 w-screen bg-purple-400 absolute top-0 -z-10'></div>
+        <div className='w-full'>
+          <h1 className='titulo-1 text-5xl'>Titulo do módulo</h1>
+          <p className='mt-4'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Dignissimos, natus.
+          </p>
+        </div>
+      </section>
+      <section className='flex flex-col items-center md:items-start md:flex-row gap-8 w-full p-4'>
+        <aside className='flex flex-col items-center gap-4 order-2 md:order-1 min-h-50 w-full min-w-50 md:w-2/5'>
+          <h2 className='font-bold text-xl'>Continue sua jornada</h2>
+          <nav className='w-full max-w-80'>
+            <ul className='flex flex-col gap-4 w-full'>
+              {[1, 2, 3, 4].map((e, index) => (
+                <li key={index}>
+                  <Link
+                    to={'e'}
+                    className='flex p-4 rounded-xl bg-gray-300 w-full gap-2'
+                  >
+                    <div className='flex gap-2 items-center'>
+                      <img src={image} alt='' className='h-12' />
+                      <div>
+                        <h3 className='font-bold mb-2'>
+                          {'module.titulo' + e}
+                        </h3>
+                        <div className='flex gap-2 items-center'>
+                          <FaAward className='text-lg' />
+                          <p className='text-sm'>
+                            <span>{'100'}</span> xp
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </aside>
+        <iframe
+          src='https://www.example.com'
+          title={'titulo do módulo'}
+          referrerPolicy='strict-origin-when-cross-origin'
+          className='w-full h-100 border border-black'
+        />
+      </section>
     </main>
   )
 }
--- src/routes/Trilhas/index.tsx
+++ src/routes/Trilhas/index.tsx
@@ -1,11 +1,20 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import image from '../../assets/images/image.png'
 
 import { FaAward } from 'react-icons/fa6'
 import { Link } from 'react-router-dom'
+import type { Trilha } from '../../types/tipoDashboard'
 
 function Trilhas () {
+  const [categoria, setCategoria] = useState('Softskills')
+  const [idTrilha, setIdTrilha] = useState('')
+  
+  useEffect(()=>{
+    const trilhaEncontrada = trilhas.find((trilha:Trilha)=> trilha.areaFoco == categoria)
+    setIdTrilha(trilhaEncontrada?.id ?? '')
+  },[])
+
   const categorias: string[] = [
     'Categoria',
     'Categoria',
@@ -20,6 +29,8 @@ function Trilhas () {
     'Categoria',
     'Categoria'
   ]
+
+  const trilhas:Trilha[] = []
 
   const modules: {
     titulo: string
@@ -68,50 +79,71 @@ function Trilhas () {
         </div>
         <div className='pt-8'>
           <h2 className='text-xl text-center font-bold'>Explorar</h2>
-          <ul className='flex justify-center gap-8 md:gap-12 w-screen max-w-240 mt-4 overflow-x-scroll no-scrollbar translate-y-1'>
-            {categorias.map((categoria, index) => (
-              <li
-                key={index}
-                className='flex flex-col items-center text-center gap-2'
-              >
-                <button onClick={() => setActive(index)}>{categoria}</button>
-                <div
-                  className={`h-2 w-[120%] ${
-                    active == index ? 'bg-gray-200' : 'bg-none'
-                  }`}
-                ></div>
-              </li>
-            ))}
-          </ul>
+          <div className='flex relative x-gradient-transparent w-screen max-w-240'>
+            <ul className='flex gap-8 md:gap-12 w-full mt-4 px-20 overflow-x-scroll no-scrollbar translate-y-1'>
+              {categorias.map((categoria, index) => (
+                <li
+                  key={index}
+                  className='flex flex-col items-center text-center gap-2'
+                >
+                  <button
+                    onClick={() => {
+                      setActive(index)
+                      setCategoria(categoria)
+                    }}
+                  >
+                    {categoria}
+                  </button>
+                  <div
+                    className={`h-2 w-[120%] ${
+                      active == index ? 'bg-gray-200' : 'bg-none'
+                    }`}
+                  ></div>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </section>
       <section className='flex justify-center w-full min-h-100 p-4 relative'>
         <div className='h-full w-screen bg-gray-800 absolute top-0 -z-10'></div>
         <ul className='flex flex-col items-center gap-4 w-full'>
-          {modules.map((module, index) => (
-            <li key={index}>
-              <Link
-                to={''}
-                className='flex p-4 rounded-xl bg-gray-300 w-full max-w-150 gap-4 hover:scale-110'
-              >
-                <div className='flex flex-col gap-2 items-center min-w-20 my-auto'>
-                  <img src={module.imagem} alt='' className='h-16 md:h-24' />
-                  <div className='flex gap-2 items-center'>
-                    <FaAward className='text-xl' />
-                    <p className='text-sm'>
-                      <span className='text-lg'>{module.xp_recompensa}</span> xp
-                    </p>
+          {modules
+            .filter(
+              (m: {
+                titulo: string
+                descricao: string
+                imagem: string
+                xp_recompensa: number
+              }) => m.titulo == idTrilha
+            )
+            .map((module, index) => (
+              <li key={index}>
+                <Link
+                  to={`/trilhas/{idTrilha}/{module.titulo.slice(10)}`}
+                  className='flex p-4 rounded-xl bg-gray-300 w-full max-w-150 gap-4 hover:scale-110'
+                >
+                  <div className='flex flex-col gap-2 items-center min-w-20 my-auto'>
+                    <img src={module.imagem} alt='' className='h-16 md:h-24' />
+                    <div className='flex gap-2 items-center'>
+                      <FaAward className='text-xl' />
+                      <p className='text-sm'>
+                        <span className='text-lg'>{module.xp_recompensa}</span>{' '}
+                        xp
+                      </p>
+                    </div>
                   </div>
-                </div>
-                <div>
                   <div>
-                    <h3 className='font-bold text-xl mb-2'>{module.titulo}</h3>
-                    <p>{module.descricao}</p>
+                    <div>
+                      <h3 className='font-bold text-xl mb-2'>
+                        {module.titulo}
+                      </h3>
+                      <p>{module.descricao}</p>
+                    </div>
                   </div>
-                </div>
-              </Link>
-            </li>
-          ))}
+                </Link>
+              </li>
+            ))}
         </ul>
       </section>
     </main>
